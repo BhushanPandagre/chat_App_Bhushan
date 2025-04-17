b88cfe72a14e1171166345ac3458945f2fdcd816
bhushan <pandagrebhushan3@gmail.com>
2025-04-17 12:41:19 +0530

    bhushan

---
+++ server/middleware/isAuthenticated.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({
+        message: "user not Authenticated.",
+      });
+    }
+    const decode = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+    console.log(decode);
+
+    if (!decode) {
+      return res.status(401).json({ message: "Invailid token" });
+    }
+    req.id = decode.userId;
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default isAuthenticated;
+
+const req = {
+  id: "",
+};
+req.id = "jhjhhjvhvvvvvvvvvvvjh";
